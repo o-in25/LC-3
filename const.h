09108b3be05c729c7510d62377b9145e499a0214
o-in25 <ehalligan12@gmail.com>
2020-08-24 20:31:32 -0400

    looking at op codes

--- const.h
+++ const.h
@@ -1,41 +1,39 @@
 #pragma once
-extern uint16_t memory[UINT16_MAX];
-extern enum Registers {
-	R_R0 = 0,
-	R_R1,
-	R_R2,
-	R_R3,
-	R_R4,
-	R_R5,
-	R_R6,
-	R_R7,
-	R_PC, /* program counter */
-	R_COND,
-	R_COUNT
+extern enum regs {
+	R_R0 = 0, // general purpose 
+	R_R1,     // general purpose 
+	R_R2,     // general purpose 
+	R_R3,     // general purpose 
+	R_R4,     // general purpose 
+	R_R5,     // general purpose 
+	R_R6,     // general purpose 
+	R_R7,     // general purpose 
+	R_PC,     // program counter 
+	R_COND,   // cond 
+	R_COUNT   // count
 };
 
-extern enum IS {
-	OP_BR = 0, /* branch */
-	OP_ADD,    /* add  */
-	OP_LD,     /* load */
-	OP_ST,     /* store */
-	OP_JSR,    /* jump register */
-	OP_AND,    /* bitwise and */
-	OP_LDR,    /* load register */
-	OP_STR,    /* store register */
-	OP_RTI,    /* unused */
-	OP_NOT,    /* bitwise not */
-	OP_LDI,    /* load indirect */
-	OP_STI,    /* store indirect */
-	OP_JMP,    /* jump */
-	OP_RES,    /* reserved (unused) */
-	OP_LEA,    /* load effective address */
-	OP_TRAP    /* execute trap */
+extern enum isa {
+	OP_BR = 0, // branch 
+	OP_ADD,    // add  
+	OP_LD,     // load 
+	OP_ST,     // store 
+	OP_JSR,    // jump register 
+	OP_AND,    // bitwise and 
+	OP_LDR,    // load register 
+	OP_STR,    // store register 
+	OP_RTI,    // unused 
+	OP_NOT,    // bitwise not 
+	OP_LDI,    // load indirect 
+	OP_STI,    // store indirect 
+	OP_JMP,    // jump 
+	OP_RES,    // reserved (unused) 
+	OP_LEA,    // load effective address 
+	OP_TRAP    // execute trap 
 };
-extern uint16_t registers[R_COUNT];
 
-extern enum Flags {
-	FL_POS = 1 << 0, /* P */
-	FL_ZRO = 1 << 1, /* Z */
-	FL_NEG = 1 << 2, /* N */
+extern enum flgs {
+	FL_POS = 1 << 0, // positive
+	FL_ZRO = 1 << 1, // zero
+	FL_NEG = 1 << 2, // negative
 };
--- virmach.cpp
+++ virmach.cpp
@@ -1,18 +1,68 @@
-// virmach.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include "const.h"
 
 int main() {
-	/* set the PC to starting position */
-	/* 0x3000 is the default */
-	enum { PC_START = 0x3000 };
-	registers[R_PC] = PC_START;
-
-	int start = 1;
-	while(start) {
-		//uint16_t instr = mem_read(registers[R_PC]++);
-		//uint16_t op = instr >> 12;
+	// memory (65536)
+	uint16_t mem[UINT16_MAX];
+	// registers (10)
+	uint16_t regs[R_COUNT];
+	// set the default 
+	regs[R_PC] = 0x3000;
+	while(true) {
+		uint16_t op = ((1 << regs[R_PC]) - 1) << 0;
+		regs[R_PC] = regs[R_PC] >> 1;
+		switch(op) {
+			case OP_BR:
+				// TODO: handle OP_BR
+				break;
+			case OP_ADD:
+				// TODO: handle OP_ADD
+				break;
+			case OP_LD:
+				// TODO: handle OP_LD
+				break;
+			case OP_ST:
+				// TODO: handle OP_ST
+				break;
+			case OP_JSR:
+				// TODO: handle OP_JSR
+				break;
+			case OP_AND:
+				// TODO: handle OP_AND
+				break;
+			case OP_LDR:
+				// TODO: handle OP_LDR
+				break;
+			case OP_STR:
+				// TODO: handle OP_STR
+				break;
+			case OP_RTI:
+				// TODO: handle OP_RTI
+				break;
+			case OP_NOT:
+				// TODO: handle OP_NOT
+				break;
+			case OP_LDI:
+				// TODO: handle OP_LDI
+				break;
+			case OP_STI:
+				// TODO: handle OP_STI
+				break;
+			case OP_JMP:
+				// TODO: handle OP_JMP
+				break;
+			case OP_RES:
+				// TODO: handle OP_RES
+				break;
+			case OP_LEA:
+				// TODO: handle OP_LEA
+				break;
+			case OP_TRAP:
+				// TODO: handle OP_TRAP
+				break;
+			default:
+				// exit
+				return 0;
+		}
 	}
 }
